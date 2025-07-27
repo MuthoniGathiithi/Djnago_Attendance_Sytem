a6d4220ce2f64ede460f4db4f3f32d63ced3cbab
Joyce <gathiithijoyce74@gmail.com>
2025-07-27 11:23:40 +0300

    fix : bug in urls.py

--- qr_attendance_system/form/urls.py
+++ qr_attendance_system/form/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.student_form_view, name='student_form'),  # this maps /form/ directly
-   path('submit_attendance/', views.submit_attendance, name='submit_attendance'),
-    
+    path('', views.submit_attendance, name='student_form'),  # this maps /form/ directly
+    path('submit_attendance/', views.submit_attendance, name='submit_attendance'),
 ]
 # qr_attendance_system/form/urls.py
 # This file defines the URL patterns for the form app, mapping the student form view to a
