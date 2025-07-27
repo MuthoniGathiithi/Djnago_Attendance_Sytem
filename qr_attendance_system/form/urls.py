5264a4786a0022c45fc263df0de4834878791996
Joyce <gathiithijoyce74@gmail.com>
2025-07-27 11:05:50 +0300

    fix: add URL pattern for submit_attendance view

--- qr_attendance_system/form/urls.py
+++ qr_attendance_system/form/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('student_form/', views.student_form_view, name='student_form'),
+   path('submit_attendance/', views.submit_attendance, name='submit_attendance'),
+    
 ]
 # qr_attendance_system/form/urls.py
 # This file defines the URL patterns for the form app, mapping the student form view to a
