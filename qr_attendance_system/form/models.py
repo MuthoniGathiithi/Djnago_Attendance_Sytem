396c77f8344bc7216d34b417f94e569b8f06d341
Joyce <gathiithijoyce74@gmail.com>
2025-07-27 09:40:42 +0300

    modifed models.py to support clas title

--- qr_attendance_system/form/models.py
+++ qr_attendance_system/form/models.py
@@ -1,1 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import StudentForm
+
+def submit_attendance(request):
+    class_title = request.GET.get('class_title', 'Unknown Class')  # default fallback
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'form/success.html', {'class_title': class_title})
+    else:
+        form = StudentForm()
+
+    return render(request, 'form/attendance_form.html', {
+        'form': form,
+        'class_title': class_title
+    })
 
