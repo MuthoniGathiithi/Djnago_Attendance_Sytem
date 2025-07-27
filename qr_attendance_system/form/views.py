e8ca61b9efd133e5b53364044f428b21027cbc2c
Joyce <gathiithijoyce74@gmail.com>
2025-07-27 09:22:01 +0300

    add submit_attendance view to handle student form submission

--- qr_attendance_system/form/views.py
+++ qr_attendance_system/form/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import StudentForm
+
+def submit_attedance(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')  # Redirect to a success page or another view
+    else:
+        form = StudentForm()
+    
+    return render(request, 'attendance_form.html', {'form': form})
 
 # Create your views here.
