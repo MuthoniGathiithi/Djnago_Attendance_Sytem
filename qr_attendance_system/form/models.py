f6bfa78c95c23cd04d7981b26cced150f9ff773e
Joyce <gathiithijoyce74@gmail.com>
2025-07-28 10:45:40 +0300

    updated form app models.py to remove it from using class school_admin

--- qr_attendance_system/form/models.py
+++ qr_attendance_system/form/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Studentform(models.Model):
     name = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
-    admin_no = models.CharField(max_length=20)
+    admin_no = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
